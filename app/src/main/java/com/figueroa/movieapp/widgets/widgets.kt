dd991c143af2021d20d2ab8753e1427096cf3f20
FigueroaGit <hmfp24@gmail.com>
2023-08-23 17:12:29 -0600

    Add coil for load images

--- app/src/main/java/com/figueroa/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/figueroa/movieapp/widgets/widgets.kt
@@ -2,11 +2,13 @@ package com.figueroa.movieapp.widgets
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -14,17 +16,25 @@ import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.figueroa.movieapp.model.Movie
+import com.figueroa.movieapp.model.getMovies
 
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
+fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -44,13 +54,32 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
                 modifier = Modifier
                     .padding(12.dp)
                     .size(100.dp),
-                shape = RectangleShape,
+                shape = CircleShape,
                 tonalElevation = 4.dp,
                 shadowElevation = 4.dp
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Images")
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current).data(movie.images[0])
+                        .crossfade(enable = true).transformations(CircleCropTransformation())
+                        .build(),
+                    contentDescription = "Movie Image Poster"
+                )
             }
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Text(
+                    text = "Director: ${movie.director}",
+                    style = MaterialTheme.typography.labelSmall
+                )
+                Text(
+                    text = "Released: ${movie.year}",
+                    style = MaterialTheme.typography.labelSmall
+                )
+            }
+
         }
     }
 }
